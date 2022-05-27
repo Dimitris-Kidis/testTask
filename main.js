41bdd14e64cc4a3a439d7700e5fe3655454c8f51
Dimitris Kidis <cyraxmite@gmail.com>
2022-05-27 10:59:29 +0300

    Shadow Bug Fixed. Edit->Completed Bug Fix. Datetime Added. Scroll Created.

--- main.js
+++ main.js
@@ -1,97 +1,248 @@
-const mainInput = document.getElementById('main-input');
-const todosWrapper = document.querySelector('.tasks');
 
-let tasks;
+    const mainInput = document.getElementById('main-input');
+    const todosWrapper = document.querySelector('.tasks');
 
-!localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'));
+    let tasks;
 
-let todoItemElems = [];
+    !localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'));
 
-function Task(description) {
-    this.description = description;
-    this.completed = false;
-}
+    let todoItemElems = [];
 
-const createTemplate = (task, index) => {
-    return `
-            <div class="task ${task.completed ? 'checked' : ''}">
-                <input type="checkbox" onclick="completeTask(${index})" id="input-checkbox" ${task.completed ? 'checked' : ''}>
-                <!-- <input type="text"  value="" readonly> -->
-                <textarea name="" id="area-title" data-id="" readonly>
-                    ${task.description}
-                </textarea>
-                <div class="buttons">
-                    <span onclick="editTask(${index})" class="edit-button">Edit</span>
-                    <span onclick="deleteTask(${index})" class="delete-button">Delete</span>
+    function Task(description) {
+        this.description = description;
+        this.completed = false;
+        this.datetime = `${setDate()}`;
+    }
+
+    function setDate() {
+        var dateObj = new Date();
+        return dateObj.getDate() + '/' + (dateObj.getMonth() + 1) + '/' + dateObj.getFullYear() + ' ' + dateObj.getHours() + ':' + dateObj.getMinutes();
+    }
+
+    const createTemplate = (task, index) => {
+        // return `
+        //         <div class="task ${task.completed ? 'checked' : ''}">
+        //             <input type="checkbox" onclick="completeTask(${index})" id="input-checkbox" ${task.completed ? 'checked' : ''}>
+        //             <textarea name="" id="area-title" data-id="" readonly>
+        //                 ${task.description}
+        //             </textarea>
+        //             <div class="buttons">
+        //                 <span onclick="editTask(${index})" class="edit-button">Edit</span>
+        //                 <span onclick="deleteTask(${index})" class="delete-button">Delete</span>
+        //             </div>
+        //         </div>
+        // `;
+        return `
+                <div class="task ${task.completed ? 'checked' : ''}">
+                    <input type="checkbox" onclick="completeTask(${index})" id="input-checkbox" ${task.completed ? 'checked' : ''}>
+                    <div class="text-and-date">
+                        <textarea name="" id="area-title" data-id="" readonly>
+                            ${task.description}
+                        </textarea>
+                        <span id="date">${task.datetime}</span>
+                    </div>
+                    <div class="buttons">
+                        <span onclick="editTask(${index})" class="edit-button">Edit</span>
+                        <span onclick="deleteTask(${index})" class="delete-button">Delete</span>
+                    </div>
                 </div>
-            </div>
-    `;
-}
-
-const filterTasks = () => {
-    const activeTasks = tasks.length && tasks.filter(item => item.completed == false);
-    const completedTasks = tasks.length && tasks.filter(item => item.completed == true);
-    tasks = [...activeTasks, ...completedTasks]
-}
-
-const fillHtmlList = () => {
-    todosWrapper.innerHTML = "";
-    if (tasks.length > 0) {
-        filterTasks();
-        tasks.forEach((item, index) => {
-            todosWrapper.innerHTML += createTemplate(item, index);
-        });
-        todoItemElems = document.querySelectorAll('.task');
+        `;
     }
-}
 
-fillHtmlList();
-
-const updateLocal = () => {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-}
-
-const completeTask = index => {
-
-    tasks[index].completed = !tasks[index].completed
-    if (tasks[index].completed) {
-        todoItemElems[index].classList.add('checked');
-    } else {
-        todoItemElems[index].classList.remove('checked');
+    const filterTasks = () => {
+        const activeTasks = tasks.length && tasks.filter(item => item.completed == false);
+        const completedTasks = tasks.length && tasks.filter(item => item.completed == true);
+        tasks = [...activeTasks, ...completedTasks]
     }
-    updateLocal();
+
+    const fillHtmlList = () => {
+        console.log('todosWrapper', todosWrapper.innerHTML)
+        todosWrapper.innerHTML = '';
+        if (tasks.length > 0) {
+            filterTasks();
+            tasks.forEach((item, index) => {
+                todosWrapper.innerHTML += createTemplate(item, index);
+            });
+            todoItemElems = document.querySelectorAll('.task');
+        }
+    }
+
+    const updateLocal = () => {
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+
+    mainInput.addEventListener('keypress', e => {
+        if (e.key === 'Enter') {
+            if (mainInput.value.length === 0) return;
+            tasks.push(new Task(mainInput.value));
+            updateLocal();
+            fillHtmlList();
+            mainInput.value = '';
+        }
+    })
+    var completeFlag = false;
+
+    const completeTask = index => {
+
+        tasks[index].completed = !tasks[index].completed
+        if (tasks[index].completed) {
+            todoItemElems[index].classList.add('checked');
+            completeFlag = true;
+        } else {
+            todoItemElems[index].classList.remove('checked');
+            completeFlag = false;
+        }
+        updateLocal();
+        fillHtmlList();
+    }
+
+    const deleteTask = index => {
+        todoItemElems[index].classList.add('delition');
+        setTimeout(() => {
+            tasks.splice(index, 1);
+            updateLocal();
+            fillHtmlList();
+        }, 500)
+    }
+    // СДЕЛАТЬ С ДАТОЙ
+    // СДЕЛАТЬ СКРОЛЛ -> Разобраться с тенью
+    // СДЕЛАТЬ Edit -> Completed Bug
+    // 
+    const editTask = index => {
+        // console.log(tasks[index].completed);
+        if ( tasks[index].completed == true ) return;
+        // if (todoItemElems[index].children[1].classList.contains('editing')) {
+        //     todoItemElems[index].children[1].readOnly = true;
+        //     todoItemElems[index].children[1].classList.remove('editing');
+        //     todoItemElems[index].children[2].children[0].style.color = '#979CA6';
+        // } else {
+        //     todoItemElems[index].children[1].readOnly = false;
+        //     todoItemElems[index].children[2].children[0].style.color = 'rgb(118, 192, 100)';
+        //     todoItemElems[index].children[1].classList.add('editing');
+        //     todoItemElems[index].children[1].focus();
+        // }
+
+
+        if (todoItemElems[index].children[1].children[0].classList.contains('editing')) {
+            todoItemElems[index].children[1].children[0].readOnly = true;
+            todoItemElems[index].children[1].children[0].classList.remove('editing');
+            todoItemElems[index].children[2].children[0].innerText = 'Edit';
+            todoItemElems[index].children[2].children[0].style.color = '#979CA6';
+            // console.log('*', todoItemElems[index].children[1].children[0])
+            tasks[index].description = todoItemElems[index].children[1].children[0].value;
+            // console.log(todoItemElems[index], tasks[index]);
+            updateLocal();
+            fillHtmlList();
+        } else {
+            todoItemElems[index].children[2].children[0].style.color = 'rgb(118, 192, 100)';
+            todoItemElems[index].children[2].children[0].innerText = 'Save';
+            todoItemElems[index].children[1].children[0].readOnly = false;
+            todoItemElems[index].children[1].children[0].classList.add('editing');
+            todoItemElems[index].children[1].children[0].focus();
+        }
+        
+        
+    }
+
     fillHtmlList();
-}
 
-mainInput.addEventListener('keypress', e => {
-    if (e.key === 'Enter') {
-        if (mainInput.value.length === 0) return;
-        tasks.push(new Task(mainInput.value));
-        updateLocal();
-        fillHtmlList();
-        mainInput.value = '';
-    }
-})
 
-const deleteTask = index => {
-    todoItemElems[index].classList.add('delition');
-    setTimeout(() => {
-        tasks.splice(index, 1);
-        updateLocal();
-        fillHtmlList();
-    }, 500)
-}
 
-const editTask = index => {
-    console.log(todoItemElems[index].children[2].children[0]);
-    if (todoItemElems[index].children[1].classList.contains('editing')) {
-        todoItemElems[index].children[1].readOnly = true;
-        todoItemElems[index].children[1].classList.remove('editing');
-        todoItemElems[index].children[2].children[0].style.color = '#979CA6';
-    } else {
-        todoItemElems[index].children[1].readOnly = false;
-        todoItemElems[index].children[2].children[0].style.color = 'rgb(118, 192, 100)';
-        todoItemElems[index].children[1].classList.add('editing');
-        todoItemElems[index].children[1].focus();
-    }
-}
+
+// const mainInput = document.getElementById('main-input');
+// const todosWrapper = document.querySelector('.tasks');
+
+// let tasks;
+
+// !localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'));
+
+// let todoItemElems = [];
+
+// function Task(description) {
+//     this.description = description;
+//     this.completed = false;
+// }
+
+// const createTemplate = (task, index) => {
+//     return `
+//             <div class="task ${task.completed ? 'checked' : ''}">
+//                 <input type="checkbox" onclick="completeTask(${index})" id="input-checkbox" ${task.completed ? 'checked' : ''}>
+//                 <!-- <input type="text"  value="" readonly> -->
+//                 <textarea name="" id="area-title" data-id="" readonly>
+//                     ${task.description}
+//                 </textarea>
+//                 <div class="buttons">
+//                     <span onclick="editTask(${index})" class="edit-button">Edit</span>
+//                     <span onclick="deleteTask(${index})" class="delete-button">Delete</span>
+//                 </div>
+//             </div>
+//     `;
+// }
+
+// const filterTasks = () => {
+//     const activeTasks = tasks.length && tasks.filter(item => item.completed == false);
+//     const completedTasks = tasks.length && tasks.filter(item => item.completed == true);
+//     tasks = [...activeTasks, ...completedTasks]
+// }
+
+// const fillHtmlList = () => {
+//     todosWrapper.innerHTML = "";
+//     if (tasks.length > 0) {
+//         filterTasks();
+//         tasks.forEach((item, index) => {
+//             todosWrapper.innerHTML += createTemplate(item, index);
+//         });
+//         todoItemElems = document.querySelectorAll('.task');
+//     }
+// }
+
+// fillHtmlList();
+
+// const updateLocal = () => {
+//     localStorage.setItem('tasks', JSON.stringify(tasks));
+// }
+
+// const completeTask = index => {
+
+//     tasks[index].completed = !tasks[index].completed
+//     if (tasks[index].completed) {
+//         todoItemElems[index].classList.add('checked');
+//     } else {
+//         todoItemElems[index].classList.remove('checked');
+//     }
+//     updateLocal();
+//     fillHtmlList();
+// }
+
+// mainInput.addEventListener('keypress', e => {
+//     if (e.key === 'Enter') {
+//         if (mainInput.value.length === 0) return;
+//         tasks.push(new Task(mainInput.value));
+//         updateLocal();
+//         fillHtmlList();
+//         mainInput.value = '';
+//     }
+// })
+
+// const deleteTask = index => {
+//     todoItemElems[index].classList.add('delition');
+//     setTimeout(() => {
+//         tasks.splice(index, 1);
+//         updateLocal();
+//         fillHtmlList();
+//     }, 500)
+// }
+
+// const editTask = index => {
+//     console.log(todoItemElems[index].children[2].children[0]);
+//     if (todoItemElems[index].children[1].classList.contains('editing')) {
+//         todoItemElems[index].children[1].readOnly = true;
+//         todoItemElems[index].children[1].classList.remove('editing');
+//         todoItemElems[index].children[2].children[0].style.color = '#979CA6';
+//     } else {
+//         todoItemElems[index].children[1].readOnly = false;
+//         todoItemElems[index].children[2].children[0].style.color = 'rgb(118, 192, 100)';
+//         todoItemElems[index].children[1].classList.add('editing');
+//         todoItemElems[index].children[1].focus();
+//     }
+// }
